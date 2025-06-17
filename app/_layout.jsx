ab310788fc96caea8c0923e32c8abedc358f99b3
Matthew King <146943749+MattEKing@users.noreply.github.com>
2025-06-17 14:04:46 -0230

    started assignment 3

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,33 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function RootLayout() {
   return(
-      <Stack>
-        <Stack.Screen name="index" options={{headerShown: true, title: "Top 3 Recipes"}}/>
-      </Stack>
-      );
+      <Tabs screenOptions={{ tabBarActiveTintColor: "#FFFFFF",tabBarInactiveTintColor: "#E8E8E8" ,tabBarStyle: {backgroundColor: "#166088"}}} >
+         <Tabs.Screen name="index"
+          options={{
+              headerShown: true,
+              headerStyle: {
+                backgroundColor: "#166088",
+              },
+              headerTitleAlign: "center",
+              headerTintColor: "#FFFFFF",
+              title: "Top 3 Recipes" ,
+              tabBarIcon: ({ color }) => <MaterialIcons size={30} name="restaurant" color={color}/>
+              }}
+          />
+        <Tabs.Screen name="update"
+            options={{
+                headerShown: true,
+                headerStyle: {
+                backgroundColor: "#166088",
+                },
+                title: "Updated Recipes",
+                headerTintColor: "#FFFFFF",
+                headerTitleAlign: "center",
+                tabBarIcon: ({ color }) => <MaterialIcons size={30} name="update" color={color}/>
+                }}
+            />
+        </Tabs>
+              );
 }
---
+++ app/update.jsx
@@ -0,0 +1,18 @@
+import { Text, TextInput, View, Button,TouchableOpacity } from "react-native";
+import {useState} from 'react';
+
+export default function Update() {
+    return (
+    <>
+        <View
+        style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+          >
+            <Text>Homer</Text>
+        </View>
+    </>
+    )
+    }
